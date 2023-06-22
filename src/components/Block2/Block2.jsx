1e1d088383ae45e2e4a576a03c5d389207bae4fb
007vict <super_vg@bk.ru>
2023-06-22 16:06:08 +0300

    feat: create block2, responsive view

--- src/components/Block2/Block2.jsx
+++ src/components/Block2/Block2.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useEffect, useState, useRef} from "react";
 import { Button, Container, Stack } from "rsuite";
 import { SectionContent } from "../SectionContent/SectionContent";
 
 import styles from "./Block2.module.scss"
+import cn from "classnames"
 
 const ImageBlock2 = () => {
   return (
-    <React.Fragment>
-      <img className={styles.imgBlock2} src={process.env.PUBLIC_URL + "/img/homeBlock2.jpeg"} alt="Home" />
-    </React.Fragment>
+    <div>
+      <img className={cn(styles.imgBlock2)} 
+                        src={process.env.PUBLIC_URL + "/img/homeBlock2.jpeg"} 
+                        alt="Home" />
+    </div>
   )
 }
 
@@ -31,9 +34,32 @@ const DescriptionBlock2 = () => {
 }
 
 const Block2 = () => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      { rootMargin: "-50%" }
+    );
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [isIntersecting]);
+
+  useEffect(() => {
+    if (isIntersecting) {
+      ref.current.querySelector(`img`).classList.add(`${styles.slidein}`);
+    } else {
+      ref.current.querySelector(`img`).classList.remove(`${styles.slidein}`);
+    }
+  }, [isIntersecting]);
+
+
   return (
     <Container className={styles.wrapper}>
-      <SectionContent>
+      <SectionContent ref={ref}>
         <ImageBlock2/>
         <DescriptionBlock2/>
       </SectionContent>
--- src/components/SectionContent/SectionContent.jsx
+++ src/components/SectionContent/SectionContent.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import cn from "classnames";
 import styles from "./SectionContent.module.scss"
 
-export const SectionContent = ({children, style, classNamess}) => {
-  return <section style={style} className={cn(styles.wrapper, [classNamess])}>
+export const SectionContent = forwardRef(({children, style, classNamess}, ref) => {
+  return <section ref={ref} style={style} className={cn(styles.wrapper, [classNamess])}>
       {children}
     </section>;
-};
+});
