76b80be92313d45fba28867e1548693672d8dba1
007vict <super_vg@bk.ru>
2023-07-31 11:38:39 +0300

    style: change height img1

--- src/components/BlockRoof/BlockRoof.jsx
+++ src/components/BlockRoof/BlockRoof.jsx
@@ -12,7 +12,7 @@ export const BlockRoof = () => {
         <Row className={styles.roww}>
           <Col className={styles.images} span={13} >
             <div>
-              <img className={styles.img1} src={process.env.PUBLIC_URL + "img/img32.jpg"} alt="img33" />
+              <img className={styles.img1} src={process.env.PUBLIC_URL + "img/img32.jpg"} alt="img32" />
             </div>
             <div>
               <img className={styles.img2} src={process.env.PUBLIC_URL + "img/2023-06-0610.33.02.jpg"} alt="img30" />
