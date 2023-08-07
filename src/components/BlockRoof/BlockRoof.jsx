bc3a19e6b605baaeeca4bc4fb0caa9ee43182e54
007vict <super_vg@bk.ru>
2023-08-07 12:25:48 +0300

    feat: responsive roof block

--- src/components/BlockRoof/BlockRoof.jsx
+++ src/components/BlockRoof/BlockRoof.jsx
@@ -10,15 +10,15 @@ export const BlockRoof = () => {
     <div>
       <SectionContent ref={refAnimation} >
         <Row className={styles.roww}>
-          <Col className={styles.images} span={13} >
+          <Col className={styles.images} lg={13} md={20} >
             <div>
-              <img className={styles.img1} src={process.env.PUBLIC_URL + "img/img32.jpg"} alt="img32" />
+              <img className={styles.img1} src={process.env.PUBLIC_URL + "img/2023-06-0610.33.02.jpg"} alt="img33" />
             </div>
             <div>
-              <img className={styles.img2} src={process.env.PUBLIC_URL + "img/2023-06-0610.33.02.jpg"} alt="img30" />
+              <img className={styles.img2} src={process.env.PUBLIC_URL + "img/img32.jpg"} alt="img30" />
             </div>
           </Col>
-          <Col className={styles.desc} span={9} >
+          <Col className={styles.desc} lg={9} md={20}>
             <h2>Крыша</h2>
             <p>Построим вашу крышу любой сложности - двухскатную, ломанную и другие виды.</p>
             <Button type={'default'} ghost size={'large'} className={styles.btn1} style={{border: "2px solid #000000", color: "#000000"}}>
